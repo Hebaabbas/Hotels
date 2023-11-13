d60f9f6309d1848eb7800d328713adfb53d046b1
Hebaabbas <124211762+Hebaabbas@users.noreply.github.com>
2023-11-13 14:33:05 +0000

    add email.js code from orignal work scandic_hotel project

---
+++ assets/js/email.js
@@ -0,0 +1,14 @@
+function sendMail(){
+    var templateParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+     
+    emailjs.send('service_u77afju', 'template_mageqwt', templateParams, "rbSvDk825LGhLJpd6")
+        .then(function(response) {
+           console.log('Success', response.status, response.text);
+        }, function(error) {
+           console.log('Failure', error);
+        });
+    }
