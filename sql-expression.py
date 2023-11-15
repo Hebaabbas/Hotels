d5dff53aa3b944b71df6950839a4003925422ee3
Hebaabbas <heba1996abbas@gmail.com>
2023-11-15 08:14:35 +0000

    define the users table with python code

--- sql-expression.py
+++ sql-expression.py
@@ -4,6 +4,19 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, Boolean, T
 db = create_engine("postgresql:///data")
 meta = MetaData(db)
 
+# Define the "users" table
+users_table = Table(
+    "users", meta,
+    Column("id", Integer, primary_key=True),
+    Column("username", String(50), nullable=False),
+    Column("email", String(100), nullable=False),
+    Column("firstname", String(100)),
+    Column("lastname", String(100)),
+    Column("is_staff", Boolean, default=False),
+    Column("is_supervisor", Boolean, default=False),
+    Column("password", String(255))
+)
+
 # Making the connection
 with db.connect() as connection:
     # Example Query - Select all users
