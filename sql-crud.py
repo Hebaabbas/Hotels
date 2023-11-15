b2ec7417ae4511f6c20ed72051beb0f4507551e1
Hebaabbas <heba1996abbas@gmail.com>
2023-11-15 11:17:37 +0000

    add class based model for the users table

--- sql-crud.py
+++ sql-crud.py
@@ -5,6 +5,18 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///data")
 base = declarative_base()
 
+# Class-based model for the "users" table
+class User(base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(100))
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    is_staff = Column(Boolean)
+    is_supervisor = Column(Boolean)
+    password = Column(String(255))
+
 Session = sessionmaker(db)
 session = Session()
 
