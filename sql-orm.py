36f19e2bc3d7569077b3e416c16ed6d64765135b
Hebaabbas <heba1996abbas@gmail.com>
2023-11-15 08:42:59 +0000

    add a class-based model for the users table in orm.py

--- sql-orm.py
+++ sql-orm.py
@@ -5,6 +5,19 @@ from sqlalchemy.orm import sessionmaker
 # Assuming the database is "data"
 db = create_engine("postgresql:///data")
 base = declarative_base()
+
+# Class-based model for the "users" table
+class User(base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(100))
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    is_staff = Column(Boolean)
+    is_supervisor = Column(Boolean)
+    password = Column(String(255))
+
 # Create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
 # Opens an actual session by calling the Session() subclass defined above
