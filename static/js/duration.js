d8f97fa016aeb0078261e54d8c32458a9049fd7c
Hebaabbas <heba1996abbas@gmail.com>
2023-12-11 19:05:54 +0000

    fix the posts js code

--- static/js/duration.js
+++ static/js/duration.js
@@ -1,13 +1,32 @@
-// Function to validate duration input
-function validateDuration(input) {
-    const durationValue = input.value;
-    const durationError = document.getElementById("durationError");
-    // Check if the duration is less than or equal to zero
-    if (durationValue <= 0) {
-        durationError.textContent = "Duration must be greater than zero.";
-        input.setCustomValidity("Duration must be greater than zero.");
-    } else {
-        durationError.textContent = "";
-        input.setCustomValidity("");
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to validate duration input
+    function validateDuration() {
+        const durationInput = document.getElementById('durationStay');
+        if (durationInput) {
+            const duration = Number(durationInput.value);
+            if (duration < 1) {
+                durationInput.setCustomValidity("Duration must be 1 day or more.");
+            } else {
+                durationInput.setCustomValidity("");
+            }
+        }
     }
-}
+
+    // Event listener for duration input change
+    const durationInput = document.getElementById('durationStay');
+    if (durationInput) {
+        durationInput.addEventListener('input', validateDuration);
+    }
+
+    // Form submission event listener
+    const reviewForm = document.querySelector('form');
+    if (reviewForm) {
+        reviewForm.addEventListener('submit', function (event) {
+            validateDuration();
+            if (!this.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+        });
+    }
+});
--- static/js/posts.js
+++ static/js/posts.js
@@ -1,39 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the dropdown element for hotel selection in the review form
-    var hotelDropdown = document.getElementById('hotelNameReview');
-    // Get the review form element
-    var form = document.querySelector('.review-form');
-
-    if (hotelDropdown && form) {
-        hotelDropdown.addEventListener('change', function () {
-            var selectedHotelId = this.value;
-            console.log('Selected Hotel ID:', selectedHotelId);
-            // Update the form's action attribute to include the selected hotel ID
-            if (selectedHotelId) {
-                form.action = form.action.replace('hotel_id=0', 'hotel_id=' + selectedHotelId);
-            }
-
-            console.log('Form Action After:', form.action);
-        });
+    // Function to validate duration input
+    function validateDuration() {
+        const durationInput = document.getElementById('durationStay');
+        const duration = parseInt(durationInput.value, 10);
+        if (duration < 1) {
+            durationInput.setCustomValidity("Duration must be 1 day or more.");
+        } else {
+            durationInput.setCustomValidity("");
+        }
     }
-});
 
+    // Add change event listener to the duration input
+    const durationInput = document.getElementById('durationStay');
+    if (durationInput) {
+        durationInput.addEventListener('change', validateDuration);
+        durationInput.addEventListener('input', validateDuration); 
+    }
 
-// Function to update the action attribute of the review form based on the selected hotel ID
-function updateAction(hotelId) {
-    // Get the review form element by its ID
-    var form = document.getElementById('reviewForm');
-    form.action = '/todo/add_review/' + hotelId + '/';
-}
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('hotelNameReview').addEventListener('change', function() {
-        // Get the selected hotel's ID
-        var hotelId = this.value;
-        var form = document.getElementById('reviewForm');
-        // Update the form's action attribute to include the selected hotel ID using Django's URL template tag
-        form.action = "{% url 'add_review' 0 %}".replace('/0/', '/' + hotelId + '/');
+    // Add submit event listener to the form
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        validateDuration();
+        if (!this.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
     });
 });
-
-
-
